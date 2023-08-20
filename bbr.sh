548415f51ac1137e70571bf94fc318dbb6c1cebc
ztan8806 <142694610+ztan8806@users.noreply.github.com>
2023-08-21 02:32:27 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # 下載安裝BBR加速
-wget "https://github.com/chiakge/Linux-NetSpeed/raw/master/tcp.sh" && chmod +x tcp.sh
-echo "1" | ./tcp.sh
+wget "https://github.com/chiakge/Linux-NetSpeed/raw/master/tcp.sh" && chmod +x tcp.sh && ./tcp.sh
+echo "1"
